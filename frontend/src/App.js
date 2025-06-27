e58e4629a38cfb9b18e2cc8bf5168243106b23e9
null <jeremynoriega101@gmail.com>
2025-06-27 02:50:21 +0000

    testing to see how much better this is

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { TempoInit } from "./tempo-init";
 
 function App() {
   return (
     <div className="App">
+      <TempoInit />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
